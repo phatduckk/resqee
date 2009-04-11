99fdd047446539ceea0873d273d9b55d769e78c9
Arin Sarkissian <arin@Halloween.local>
2009-04-10 22:36:52 -0700

    forgot to add the plugin stuff:

---
+++ inc/Resqee/Plugin/Events.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Class that holds values for plugin events. All of the key/value pairs
+ * will be turned into constants in Resqee_Config::__construct()
+ */
+class Resqee_Plugin_Events
+{
+    const SERVER_JOB_POST = 'SERVER_JOB_POST';
+}
+
+?>
